a2a3b2565ed291203bb54b37f983b82cbc31cc28
gxf <476402862@qq.com>
2025-01-16 11:22:34 +0800

    third commit

--- src/main/java/com/eqixi/gxf/GitTest.java
+++ src/main/java/com/eqixi/gxf/GitTest.java
@@ -8,6 +8,7 @@ package com.eqixi.gxf;
 public class GitTest {
  public static void main(String[] args) {
      System.out.println("hello git");
-     System.out.println("hello git");
+     System.out.println("hello git2");
+     System.out.println("hello git3");
  }
 }
