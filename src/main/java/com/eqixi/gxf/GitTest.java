881a39e5d97a22cf452986e5d371c5626b2eec8d
gxf <476402862@qq.com>
2025-01-16 11:14:53 +0800

    idea 第一次

---
+++ src/main/java/com/eqixi/gxf/GitTest.java
@@ -0,0 +1,12 @@
+package com.eqixi.gxf;
+
+/**
+ * IntelliJ IDEA com.eqixi.gxf
+ * Administrator
+ * 2025 16/01  11:11
+ */
+public class GitTest {
+ public static void main(String[] args) {
+     System.out.println("hello git");
+ }
+}
