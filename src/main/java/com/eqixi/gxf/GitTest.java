36751e7b67a5b60fc8f61e2b9bbf98d367b96d02
gxf <476402862@qq.com>
2025-01-18 15:34:14 +0800

    github

--- src/main/java/com/eqixi/gxf/GitTest.java
+++ src/main/java/com/eqixi/gxf/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
      System.out.println("hello git3");
      System.out.println("hello git hot-fix");
      System.out.print("github");
+     System.out.println("github.com");
  }
 }
